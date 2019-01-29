2ed9eefb1ea3fff6efffb6aa12fc8e6787745c7a
hbb0b0@163.com <hbb0b0@163.com>
2019-01-29 11:03:55 +0800

    commit

---
+++ API002/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API002.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"API002:{DateTime.Now.ToString()}  { Environment.MachineName + " OS:" + Environment.OSVersion.VersionString}";
+        }
+
+        [HttpGet("/health")]
+        public IActionResult Heathle()
+        {
+            return Ok();
+        }
+    }
+}
